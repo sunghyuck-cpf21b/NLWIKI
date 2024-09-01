4e52332c8feaf8423bdde2a6888c6a248af61692
양성혁 <subuncreamm@gmail.com>
2024-09-02 01:04:51 +0900

    style fix 202409020104

--- frontend/src/routes/UserLogin.svelte
+++ frontend/src/routes/UserLogin.svelte
@@ -29,18 +29,38 @@
     }
 </script>
 
-<div>
+<div class='login_box'>
     <h5>로그인</h5>
     <Error error={error} />
     <form method='post'>
-        <div>
+        <div class='login_box'>
             <label for='username'>사용자 이름</label>
             <input type='text' id='username' bind:value='{login_username}'>
         </div>
-        <div>
+        <div class='login_box'>
             <label for='password'>비밀번호</label>
             <input type='password' id='password' bind:value='{login_password}'>
         </div>
         <button type='submit' on:click='{login}'>로그인</button>
     </form>
 </div>
+
+
+<style>
+    .login_box {
+        border: 1px solid #000000;
+        width: 400px;
+        display: flex;
+        height: 300px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        height: 200px;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+</style>
