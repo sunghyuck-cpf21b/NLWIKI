1c16c6917166c109434898b14ce81c25c1e88fc4
양성혁 <subuncreamm@gmail.com>
2024-09-07 22:54:17 +0900

    fix reereresend

--- NtoP.py
+++ NtoP.py
@@ -9,5 +9,5 @@ ns = db.query(Nonlan).all()
 for n in ns:
     p = Post(id=n.id, category='논란', subject=n.subject, content=n.content, create_date=n.create_date,
              person=n.person, occ_date=n.occ_date, user_id=n.user_id)
-    p.add(p)
-    p.commit()
+    db.add(p)
+    db.commit()
